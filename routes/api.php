52cf79357b0491cacaf2a13ce3c7a10a2e8e53f0
rifqiazib <muhammad.zakaria2108@gmail.com>
2024-12-09 15:54:12 +0700

    [fix] positin routing

--- routes/api.php
+++ routes/api.php
@@ -12,26 +12,29 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+
+Route::middleware([JwtMiddleware::class])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-});
 
-Route::middleware([JwtMiddleware::class])->prefix('medicine')->group(function () {
+    // medicine section
+    Route::group(['prefix' => 'medicine'], function () {
+        
+        // Category Section
+        Route::group(['prefix' => 'category'], function () {
+            Route::get('/', [MedicineCategoryController::class, 'index'])->name('medicine.category.index');
+            Route::post('/store', [MedicineCategoryController::class, 'store'])->name('medicine.category.create');
+            Route::get('/{id}', [MedicineCategoryController::class, 'show'])->name('medicine.category.show');
+            Route::put('/update/{id}', [MedicineCategoryController::class, 'update'])->name('medicine.category.update');
+            Route::delete('/delete/{id}', [MedicineCategoryController::class, 'destroy'])->name('medicine.category.delete');
+        });
 
-    Route::get('/', [MedicineController::class, 'index'])->name('medicine.index');
-    Route::post('/store', [MedicineController::class, 'store'])->name('medicine.store');
-    Route::get('/{id}', [MedicineController::class, 'show'])->name('medicine.show');
-    Route::put('/update/{id}', [MedicineController::class, 'update'])->name('medicine.update');
-    Route::delete('/delete/{id}', [MedicineController::class, 'destroy'])->name('medicine.delete');
-
-    // Category Section
-    Route::group(['prefix' => 'category'], function () {
-        Route::get('/', [MedicineCategoryController::class, 'index'])->name('medicine.category.index');
-        Route::post('/store', [MedicineCategoryController::class, 'store'])->name('medicine.category.create');
-        Route::get('/{id}', [MedicineCategoryController::class, 'show'])->name('medicine.category.show');
-        Route::put('/update/{id}', [MedicineCategoryController::class, 'update'])->name('medicine.category.update');
-        Route::delete('/delete/{id}', [MedicineCategoryController::class, 'destroy'])->name('medicine.category.delete');
+        Route::get('/', [MedicineController::class, 'index'])->name('medicine.index');
+        Route::post('/store', [MedicineController::class, 'store'])->name('medicine.store');
+        Route::get('/{id}', [MedicineController::class, 'show'])->name('medicine.show');
+        Route::put('/update/{id}', [MedicineController::class, 'update'])->name('medicine.update');
+        Route::delete('/delete/{id}', [MedicineController::class, 'destroy'])->name('medicine.delete');
     });
 });
